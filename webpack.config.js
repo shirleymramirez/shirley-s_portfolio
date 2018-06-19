d437a931f576e0f84e437e09e536a1881433dcc0
Shirley Ramirez <shirley.mramirez@yahoo.com>
2018-06-19 13:34:57 -0700

    added webpack config and dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
 module.exports = {
   module: {
     rules: [
@@ -7,7 +14,26 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [htmlWebpackPlugin]
 };
