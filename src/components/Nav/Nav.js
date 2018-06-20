54a34f4c47d0329e7ae34abfd115e592c85efadf
Shirley Ramirez <shirley.mramirez@yahoo.com>
2018-06-19 20:12:40 -0700

    added navbar components

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Navbar, NavItem } from "react-materialize";
+import { Link } from "react-router-dom";
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <div>
+      <Navbar brand="Shirley Ramirez" right>
+        <div className="link-navbar">
+          <NavItem href="home.html">HOME</NavItem>
+          <NavItem href="portfolio.html">PORTFOLIO</NavItem>
+          <NavItem href="contact-me.html">CONTACT ME</NavItem>
+        </div>
+      </Navbar>
+    </div>
+  );
+};
+export default Nav;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Nav from '../../components/Nav';
+import Footer from '../../components/Footer';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Nav />
+            <Footer />
+        </div>
+        
+    );
+};
+
+export default Home;
