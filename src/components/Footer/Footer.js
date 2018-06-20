a6e0e65828f3f5222a27387ece771715ae1a8d60
Shirley Ramirez <shirley.mramirez@yahoo.com>
2018-06-19 23:42:33 -0700

    added footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,28 +3,13 @@ import { Footer } from "react-materialize";
 import "./Footer.css";
 
 const FooterContent = () => {
-  return ( 
-    <Footer copyrights="© 2018 Copyright Shirley Ramirez"
-        moreLinks={<a className="grey-text text-lighten-4 right" href="#!">
-          <a className="grey-text text-lighten-4 right">
-            <h6>Email me at:</h6>
-            <a href="mailto:shirley.mramirez@yahoo.com?Subject=Hello%20Shirley" target="_top">
-              shirley.mramirez@yahoo.com
-            </a>
-          </a>
-          <a className="grey-text text-lighten-4 right">
-            <a href="https://www.linkedin.com/in/shirley-ramirez/" target="_blank" rel="noopener noreferrer">
-              Linkedln
-            </a>
-          </a>
-
-          <a className="grey-text text-lighten-4 right">
-            <a href="https://github.com/shirleymramirez" target="_blank" rel="noopener noreferrer">
-              GitHub
-            </a>
-          </a>
-        </a>} />
-  );
+  return (
+    <Footer copyrights="© 2018 Copyright Shirley Ramirez" 
+        moreLinks={<a className="grey-text text-lighten-4 right"  
+            href="mailto:shirley.mramirez@yahoo.com?Subject=Hello%20Shirley" target="_top">
+            shirley.mramirez@yahoo.com
+        </a>} /> 
+    ); 
 };
 
 export default FooterContent;
