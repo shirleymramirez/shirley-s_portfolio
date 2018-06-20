1cce6d07e87a2cb493209a81a9a5c8a3506d3657
Shirley Ramirez <shirley.mramirez@yahoo.com>
2018-06-20 00:13:03 -0700

    fixing footer style and added carousel

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Nav from '../../components/Nav';
 import Footer from '../../components/Footer';
-
+import Carousel from '../../components/Carousel';
 
 const Home = () => {
     return (
         <div>
             <Nav />
+            <Carousel />
             <Footer />
-        </div>
-        
+        </div>  
     );
 };
 
