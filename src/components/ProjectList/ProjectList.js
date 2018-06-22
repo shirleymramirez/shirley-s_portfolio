9028fcd45132c9b7354590e200d8b8f8c1171349
Shirley Ramirez <shirley.mramirez@yahoo.com>
2018-06-21 18:51:56 -0700

    added link of some of my projects to ProjectList

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Card, CardTitle } from "react-materialize";
+import { Card, CardTitle } from "react-materialize";
 
 const ProjectList = () => {
     return <div>
@@ -14,6 +14,75 @@ const ProjectList = () => {
             </p>}>
           <p>
             <a href="https://agile-stream-27904.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+              Click Me to see working App
+            </a>
+          </p>
+        </Card>
+
+        <Card header={<CardTitle reveal image={"../../images/Stna.png"} waves="light" alt="Scraped-that-News-App" />} title="Scraped-that-News-App" reveal={<p
+            >
+              A web app that lets users view and leave comments on the
+              latest news. Tech used: Mongoose and Cheerio
+            </p>}>
+          <p>
+            <a href="https://evening-stream-73257.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+              Click Me to see working App
+            </a>
+          </p>
+        </Card>
+
+        <Card header={<CardTitle reveal image={"../../images/Cta.png"} waves="light" alt="Customer-Ticketing-App" />} title="Customer-Ticketing-App" reveal={<p
+            >
+              App to facilitate service for Agents and Clients using chat
+              rooms and text messages. Tech used: twilio, socket.io,
+              sequelize, express, etc.
+            </p>}>
+          <p>
+            <a href="https://esm-customer-support-app.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+              Click Me to see working App
+            </a>
+          </p>
+        </Card>
+
+        <Card header={<CardTitle reveal image={"../../images/Edb.png"} waves="light" alt="Eat-da-Burger" />} title="Eat-da-Burger" reveal={<p
+            >
+              A restaurant app that lets users input the names of burgers
+              they'd like to eat. Tech used: MySQL, Node, Express,
+              Handlebars and sequelize.
+            </p>}>
+          <p>
+            <a href="https://morning-falls-11723.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+              Click Me to see working App
+            </a>
+          </p>
+        </Card>
+
+        <Card header={<CardTitle reveal image={"../../images/Liri.png"} waves="light" alt="liri-node-app" />} title="liri-node-app" reveal={<p
+            >
+              A command line node app that takes in parameters and gives you
+              back data. LIRI is a node app which stands for Language
+              Interpretation and Recognition Interface. It is like SIRI
+              (from an iphone). Tech used: Nodejs, tweeter api, spotify api,
+              ombd api
+            </p>}>
+          <p>
+            <a href="https://github.com/shirleymramirez/liri-node-app" target="_blank" rel="noopener noreferrer">
+              Click Me to see working App
+            </a>
+          </p>
+        </Card>
+
+        <Card header={<CardTitle reveal image={"../../images/ff.png"} waves="light" alt="FriendFinder" />} title="FriendFinder" reveal={<p
+            >
+              Node and Express Servers dating app. This full-stack site will
+              take in results from your users' surveys, then compare their
+              answers with those from other users. The app will then display
+              the name and picture of the user with the best overall match.
+              Tech used: Express, body-parser, path, javascript, jquery,
+              ajax, nodejs
+            </p>}>
+          <p>
+            <a href="https://young-coast-19729.herokuapp.com/" target="_blank" rel="noopener noreferrer">
               Click Me to see working App
             </a>
           </p>
