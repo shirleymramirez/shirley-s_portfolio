a68efdbfe0596ac44bfb221593adc6c65325bd93
unknown <shirley.mramirez@yahoo.com>
2018-07-03 22:43:36 -0700

    working with webpack and css bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const webpack = require('webpack');
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
 
 module.exports = {
-    entry: path.join(__dirname, "./src/index.js"),
+    entry: path.join(__dirname, "./src"),
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js'
@@ -18,37 +21,53 @@ module.exports = {
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
-                }
+                },
+                // include: path.resolve('/src')
             },
             {
                 test: /\.css$/,
-                use: [{
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true,
-                            importLoaders: 1,
-                            localIdentName: "[name]_[local]_[hash:base64]",
-                            sourceMap: true,
-                            minimize: true,
-                            styles: {
-                                'materialize': true,
-                            },
-                        }
-                    }
-                ]
-            },
-            
-                { 
-                    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-                    loader: 'url-loader?limit=10000&mimetype=application/font-woff' 
+                // include: __dirname + '/src',
+                use: [
+                        {
+                            loader: "style-loader"
+                        },
+                        {
+                            loader: "css-loader",   
+                            // loader: ExtractTextPlugin.extract("css-loader"), 
+                            options: {
+                                modules: true,
+                                importLoaders: 1,
+                                localIdentName: "[name]_[local]_[hash:base64]",
+                                sourceMap: true,
+                                minimize: true,
+                                // styles: {
+                                //     'materialize': true,
+                                // },
+                             },
+                        },
+                    ],
                 },
-                { 
-                    test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-                    loader: 'file-loader' 
-                }
+            { 
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff' 
+            },
+            { 
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+                loader: 'file-loader' 
+            },
+            {
+                test: /\.gif/,
+                loader: "url-loader?limit=10000&mimetype=image/gif"
+            },
+            {
+                test: /\.jpg/,
+                loader: "url-loader?limit=10000&mimetype=image/jpg"
+            },
+            {
+                test: /\.png/,
+                loader: "url-loader?limit=10000&mimetype=image/png"
+            }
+        
         ]
     },
 
@@ -61,13 +80,9 @@ module.exports = {
                 'window.$': 'jquery',
                 'window.jquery': 'jquery'
             }
-        )
+        ),
+        // new ExtractTextPlugin("styles.css")
     ],
-    //   externals: [{
-            // materializecss: 'node-modules/materialize-css/dist/css/materialize.min.css',
-            // materialize: 'node-modules/materialize-css/dist/js/materialize.min.js',
-            // jquery:'node-modules/jquery/dist/jquery-min.js'
-    //  }]
 
     resolve: {
          alias: {
